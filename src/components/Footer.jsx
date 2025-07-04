f626273b5273aec60ffbb65cc5585e0d950c8666
endemic420 <karanjadennis17@gmail.com>
2025-07-04 23:15:03 +0300

    second official commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         &copy; {new Date().getFullYear()} TaskManager. All rights reserved.
       </div>
       <div className="mt-2">
-        <a href="https://github.com/your-repo" className="underline">GitHub</a>
+        <a href="https://github.com/PLP-MERN-Stack-Development/week-3-react-js-assignment-endemic420.git" className="underline">GitHub</a>
       </div>
     </footer>
   );
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,6 @@
 function About(){
     return(
-        <div className= "h-full w-full bg-gray-100 dark:bg-transparent mx-auto p-4">
+        <div className= "h-full w-full bg-white dark:bg-transparent mx-auto p-4">
             <h1 className="text-4xl font-bold mb-4 text-gray-900 dark:text-white">About the Task Manager</h1>
             <p className="text-gray-800 dark:text-gray-400">
                 The Task Manager is a web application that manages users tasks efficiently.The app allows the user to add, delete, and update tasks as needed. It also marks the tasks that are completed.
